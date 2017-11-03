660e3cafd86483e1c5180f2d46a8d0ab257c27c5
book <book@book-desktop.(none)>
2017-11-04 01:54:15 +0800

    avoid zombie 2 failed

--- chapter8/8-8.c
+++ chapter8/8-8.c
@@ -5,19 +5,23 @@ int main()
 {
 	pid_t pid;
 	
-	if((pid = fork()) < 0){
+	if((pid = fork()) < 0){              
 		err_sys("fork error\n");
 	}
-	else if(pid == 0){
+	else if(pid == 0){                    //first child
 		if((pid = fork()) < 0)
 			err_sys("fork error\n");
-		else if(pid > 0)
+		else if(pid > 0){             //first child
+		   	printf("first child, pid = %ld\n",(long)getpid());
 			exit(0);
-	sleep(2);
-	printf("second child, parent pid = %ld\n",(long)getpid());
-	exit(0);
+		}
+		else{	
+			sleep(4);
+		   	printf("second child, pid = %ld\n",(long)getpid());
+		}
+		printf("second child, parent pid = %ld\n",(long)getpid());
+		exit(0);
 	}
-	
 	if(waitpid(pid,NULL,0) != pid)
 		err_sys("waitpid error\n");
 	
