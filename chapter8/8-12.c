564e6068015eb6db65c0dc43dff9601d85fe09f4
book <book@book-desktop.(none)>
2017-11-04 03:23:38 +0800

    with PV, parent first

--- chapter8/8-12.c
+++ chapter8/8-12.c
@@ -5,13 +5,19 @@ void charactatime(char *str);
 int main(void)
 {
 	pid_t pid;
-	
+
+	TELL_WAIT();	
+
 	if((pid = fork()) < 0)
 		err_sys("fork error\n");
-	else if(pid == 0)
+	else if(pid == 0){
+		WAIT_PARENT();
 		charactatime("output from child\n");
-	else
+	}
+	else{
 		charactatime("output from parent\n");
+		TELL_CHILD(pid);
+	}
 	
 	exit(0);
 }
