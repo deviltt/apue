d371c50503bb16843de3aa61a8e7b9b175f774ab
deviltt <tt416926063@qq.com>
2017-12-18 08:52:13 +0800

    SIGCLD and SIGCHLD are the same in Linux

---
+++ chapter10/10-6.c
@@ -0,0 +1,37 @@
+#include "apue.h"
+#include <sys/wait.h>
+
+void sig_cld(int signum)
+{
+	pid_t pid;
+	int status;
+
+	printf("SIGCLD received\n");
+	
+	if(signal(SIGCLD, sig_cld) == SIG_ERR)
+		perror("signal error");
+
+	if((pid = wait(&status)) < 0)
+		perror("wait error");
+
+	printf("pid = %d\n", pid);
+}
+
+int main()
+{
+	pid_t pid;
+
+	if(signal(SIGCLD, sig_cld) == SIG_ERR)
+		perror("signal error");
+	
+	if((pid = fork()) < 0){
+		perror("fork error");
+	}
+	else if(pid == 0){
+		sleep(2);
+		_exit(0);
+	}
+		
+	pause();
+	exit(0);
+}
